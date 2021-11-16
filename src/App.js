1f7215d6549bb94cf079201721d0d500dee46263
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2021-11-16 15:18:00 +0100

    Add unsplash api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import Navbar from './components/Navbar';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 import './App.css';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [clientID, setClientID] = useState(
+    process.env.REACT_APP_UNSPLASH_ACCESS_KEY
+  );
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${term}&client_id=${clientID}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [clientID, term]);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 className='2xl: bg-green-400'>Oussama</h1>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={`${darkTheme && 'dark-mode'}`}>
+      <Navbar toggleTheme={toggleTheme} />
+      <div className='container mx-auto px-4 md:px-12'>
+        <ImageSearch searchText={(text) => setTerm(text)} />
+
+        {!isLoading && images.length === 0 && (
+          <h1 className='text-5xl text-center mx-auto mt-32'>
+            No Images Found
+          </h1>
+        )}
+
+        {isLoading ? (
+          <h1 className='text-6xl text-center mx-auto mt-32'>Loading...</h1>
+        ) : (
+          <div className='flex flex-wrap -mx-1 lg:-mx-4'>
+            {images.map((image) => (
+              <ImageCard key={image.id} image={image} />
+            ))}
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
 
